8e52e2f2f9721eb213a32e9d2f77384a877ec092
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-02-15 23:55:07 -0800

    better error checking, getDevices function

--- daemon/src/getDeviceManager.js
+++ daemon/src/getDeviceManager.js
@@ -5,30 +5,39 @@ const getDeviceManager = slaves => {
 
   const devices = { };
 
-  const addDevice = (deviceType, deviceIdentifierOptions) => {
+  const addDevice = (deviceType, reachabilityInfo) => {
+    if (typeof(deviceType) !== 'string' || typeof(reachabilityInfo) !== 'string'){
+      throw (new Error('invalid arguments to addDevice'));
+    }
+
     const slave = slaves[deviceType];
     if (slave === undefined){
       throw (new Error('device type not supported'));
     }
-    if (!slave.isValidReachabilityInfo(deviceIdentifierOptions.resourceInfo)){
+    if (!slave.isValidReachabilityInfo(reachabilityInfo)){
       throw (new Error('device type has bad reachability info'));
     }
 
     devices[uuid()] = ({
       type: deviceType,
-      info: deviceIdentifierOptions.resourceInfo,
+      info: reachabilityInfo,
     });
 
   };
   const removeDevice = id => {
+    if (typeof(id) !== 'string'){
+      throw (new Error('id must be string in remove device'));
+    }
+
     delete devices[id];
   };
-  const listDevicesAsString = () => JSON.stringify(devices);
+
+  const getDevices = () => JSON.parse(JSON.stringify(devices));
 
   const deviceManager = {
     addDevice,
     removeDevice,
-    listDevicesAsString,
+    getDevices,
   };
 
   return deviceManager;
