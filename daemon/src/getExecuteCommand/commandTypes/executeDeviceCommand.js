bde4d59a9b27c016ac67fd46788f69c659197d25
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-02-18 22:42:28 -0800

    remove device working

--- daemon/src/getExecuteCommand/commandTypes/executeDeviceCommand.js
+++ daemon/src/getExecuteCommand/commandTypes/executeDeviceCommand.js
@@ -13,11 +13,15 @@ const executeList  = deviceManager => {
 
 const executeAdd = (deviceManager, deviceType, deviceReachabilityInfo) => deviceManager.addDevice(deviceType, deviceReachabilityInfo);
 
+const executeRemove = (deviceManager, deviceId) => deviceManager.removeDevice(deviceId);
+
 const executeDeviceCommand = (command, deviceManager) => {
   if (command.option.type === 'list'){
    executeList(deviceManager);
   }else if (command.option.type === 'add'){
     executeAdd(deviceManager, command.option.name, command.option.reach);
+  }else if (command.option.type === 'delete'){
+    executeRemove(deviceManager, command.option.id);
   }
 };
 
