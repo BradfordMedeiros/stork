b0e73401d822dbe727260af3cb0e3ac32334dd51
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-02-21 23:39:23 -0800

    list command by device id, type implemented

--- daemon/src/getExecuteCommand/commandTypes/executeCommandCommand.js
+++ daemon/src/getExecuteCommand/commandTypes/executeCommandCommand.js
@@ -6,10 +6,15 @@ const executeCommandCommand = (command,  commandManager) => {
     console.log('device command')
   }else if (command.option.type === 'group'){
     console.log('group command');
-  }else if (command.option.type === 'list_device'){
-    const deviceType = command.option.device;
-    const commands = commandManager.listCommandsForDeviceByDeviceType(deviceType);
-    console.log(commands);
+  }else if (command.option.type === 'list_by_type'){
+    const device_type = command.option.device;
+    const commands = commandManager.listCommandsForDeviceByDeviceType(device_type);
+    console.log(`Commands for device type [${device_type}]: `,commands);
+  }
+  else if (command.option.type === 'list_device'){
+    const deviceId = command.option.device;
+    const commands = commandManager.listCommandsForDeviceById(deviceId);
+    console.log(`Commands for device id [${deviceId}]: `, commands);
   }else if (command.option.type === 'list_group'){
     console.log('command: list group')
   }
