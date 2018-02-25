989940f4d1f7314e04184bf4c74b38e79c11abd2
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-02-24 21:30:01 -0800

    config manager tests

--- daemon/tests/configManager.test.js
+++ daemon/tests/configManager.test.js
@@ -12,6 +12,30 @@ describe('config manager', () => {
     assert.equal(true, configManager.isValidDeviceConfiguration('mock_slave', '{ "somefield": true }'));
     assert.equal(false, configManager.isValidDeviceConfiguration('mock_slave', 'invalid config'));
   });
-  it('can configure a device');
-  it('will fail to configure a device is the config is invalid');
+  it('can configure a device', async () => {
+    const deviceManager = getDeviceManager(mockSlaves);
+    const configManager = getConfigManager({ deviceManager, slaves: mockSlaves });
+    const deviceId = deviceManager.addDevice('mock_slave', '500');
+
+    let hasErrored = false;
+    try{
+      await configManager.configureDevice(deviceId, '{ "somefield" : true }')
+    }catch(e){
+      hasErrored = true;
+    }
+    assert.equal(hasErrored, false);
+  });
+  it('will fail to configure a device is the config is invalid', async () => {
+    const deviceManager = getDeviceManager(mockSlaves);
+    const configManager = getConfigManager({ deviceManager, slaves: mockSlaves });
+    const deviceId = deviceManager.addDevice('mock_slave', '500');
+
+    let hasErrored = false;
+    try{
+      await configManager.configureDevice(deviceId, 'invalid configuration')
+    }catch(e){
+      hasErrored = true;
+    }
+    assert.equal(hasErrored, true);
+  });
 });
