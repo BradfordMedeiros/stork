a6ae5a92f981daff5b3ed3c1c6363ed61706dfbe
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-02-28 23:34:26 -0800

    status manager

--- daemon/src/executeStorkCommand.js
+++ daemon/src/executeStorkCommand.js
@@ -3,6 +3,7 @@ const getDeviceManager = require('./util/managers/getDeviceManager');
 const getGroupManager = require('./util/managers/getGroupManager');
 const getCommandManager = require('./util/managers/getCommandManager');
 const getConfigManager = require('./util/managers/getConfigManager');
+const getStatusManager = require('./util/managers/getStatusManager');
 const getExecuteCommand = require('./util/getExecuteCommand/getExecuteCommand');
 const parseCommand = require('./util/parseCommand/parseCommand');
 
@@ -12,6 +13,7 @@ const executeCommand  = commandString => {
   const groupManager = getGroupManager('./persistData/savedGroups');
   const commandManager = getCommandManager({ deviceManager, groupManager, slaves });
   const configManager = getConfigManager({ deviceManager, slaves });
+  const statusManager = getStatusManager({ deviceManager, slaves });
   const executeCommand = getExecuteCommand({ deviceManager, groupManager, commandManager, configManager });
   const command = parseCommand(commandString);
   executeCommand(command);
--- daemon/src/util/managers/getStatusManager.js
+++ daemon/src/util/managers/getStatusManager.js
@@ -1,3 +1,27 @@
-/**
- * Created by brad on 2/24/18.
- */
+
+const getStatusManager = ({ deviceManager, slaves }) => {
+  if (deviceManager === undefined){
+    throw (new Error('StatusManager: deviceManager is not defined'));
+  }
+  if (slaves === undefined){
+    throw (new Error('StatusManager: slaves is not defined'));
+  }
+
+  const getDeviceStatus = async deviceId => {
+    if (deviceId === undefined){
+      throw (new Error('invalid parameters'));
+    }
+
+    const deviceType = deviceManager.getDeviceById(deviceId).type;
+    const getStatus = slaves[deviceType].status;
+    return await getStatus();
+  };
+
+  const statusManager = {
+    getDeviceStatus,
+  };
+
+  return statusManager;
+};
+
+module.exports = getStatusManager;
