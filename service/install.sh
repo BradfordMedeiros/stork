342f9b58a66d83c617dd6b763a40132579080591
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-03-01 22:38:53 -0800

    stork client on install

--- service/install.sh
+++ service/install.sh
@@ -1,9 +1,15 @@
+# install daemon dependencies
 (
    cd ../daemon/ 
    yarn install
 )
+
+# copy stork into opt/storkd and setup systemd service
 sudo mkdir -p /opt/storkd/
 sudo cp -r .. /opt/storkd
 sudo cp storkd /etc/init.d
 sudo systemctl daemon-reload 
 sudo systemctl start storkd
+
+# setup client script
+sudo cp ../client/stork.sh /usr/bin/stork
--- service/uninstall.sh
+++ service/uninstall.sh
@@ -3,3 +3,4 @@ sudo service storkd stop
 sudo rm -rf /opt/storkd
 sudo rm -rf /etc/init.d/storkd
 sudo systemctl daemon-reload 
+sudo rm /usr/bin/stork
